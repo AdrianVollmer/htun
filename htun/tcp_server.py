e9d8c9e80e9858931ba28d14ef671823eb3efd7b
Adrian Vollmer <adrian.vollmer@syss.de>
2019-02-24 19:44:39 +0100

    remove TCP_NODELAY

--- htun/tcp_server.py
+++ htun/tcp_server.py
@@ -7,7 +7,6 @@ def create_socket():
     logging.info("Waiting for connection... ", end='')
     _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     _sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    _sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     _sock.bind((args.bindip, args.lport))
     _sock.listen(1)
     server_socket, a = _sock.accept()
